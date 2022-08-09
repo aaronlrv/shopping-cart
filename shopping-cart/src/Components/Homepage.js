94b0dec8bce93de7ba8c1e9c7c5e5f9e128d5d9e
aaronlrv <aaronvitharanage@gmail.com>
2022-08-09 20:34:51 +0400

    Add hero text

--- shopping-cart/src/Components/Homepage.js
+++ shopping-cart/src/Components/Homepage.js
@@ -1,7 +1,15 @@
 function Homepage() {
   return (
     <div class="h-screen bg-[url('/home/aaron/odin-projects/shopping-cart/shopping-cart/src/bg.jpg')]">
-      <div>Hello</div>
+      <div
+        className="Title"
+        class="h-full w-full flex flex-col justify-center items-center gap-5"
+      >
+        <h3 class="text-7xl text-white ">Fortnite Webpage Name</h3>
+        <p class="text-2xl text-white ">
+          The best place to buy your fornite cosmetics.
+        </p>
+      </div>
     </div>
   );
 }
