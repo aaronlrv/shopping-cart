76598c27d2af907060e4e600ea34ad5c4cf5b897
aaronlrv <aaronvitharanage@gmail.com>
2022-08-09 19:29:52 +0400

    Initialize create react app + tailwind

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p class="bg-sky-200"> Hi</p>
+    </div>
+  );
+}
+
+export default App;
