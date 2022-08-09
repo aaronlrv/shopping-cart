82abf3db6033786501735fefe453e15246f3363d
aaronlrv <aaronvitharanage@gmail.com>
2022-08-09 19:38:57 +0400

    Add background to homepage

---
+++ shopping-cart/src/Components/Homepage.js
@@ -0,0 +1,9 @@
+function Homepage() {
+  return (
+    <div class="h-screen bg-[url('/home/aaron/odin-projects/shopping-cart/shopping-cart/src/bg.jpg')]">
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default Homepage;
