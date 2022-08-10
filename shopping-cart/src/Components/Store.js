4ba1f3754e3a31dba349f1cf72d0052e30185e99
aaronlrv <aaronvitharanage@gmail.com>
2022-08-10 11:56:14 +0400

    Allow items API to be called

--- shopping-cart/src/Components/Store.js
+++ shopping-cart/src/Components/Store.js
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 function Store() {
+  let [items, setItems] = useState();
+
+  useEffect(() => {
+    async function getItems() {
+      let dataItems = await fetch(
+        "https://fortnite-api.theapinetwork.com/store/get",
+        { mode: "cors" }
+      );
+
+      let dataJson = await dataItems.json();
+      console.log(dataJson);
+      setItems(dataItems);
+    }
+
+    getItems();
+  }, []);
+
   return (
     <div>
       <p>HI! This is the Store page</p>
