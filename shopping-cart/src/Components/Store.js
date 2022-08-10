c535132adb0d5f28b734094dd6a92be14eca73fd
aaronlrv <aaronvitharanage@gmail.com>
2022-08-10 16:54:56 +0400

    Add animations

--- shopping-cart/src/Components/Store.js
+++ shopping-cart/src/Components/Store.js
@@ -66,7 +66,7 @@ function Store() {
         >
           <div
             className="card"
-            class=" border-solid border-slate-300 h-72 gap-6 m-5 border-2 grid grid-rows-[100,100] auto-rows-[100px]  grid-cols-2 shadow-xl "
+            class=" border-solid border-slate-300 h-72 gap-6 m-5 border-2 grid grid-rows-[100,100] auto-rows-[100px]  grid-cols-2 shadow-xl transition ease-in-out delay-100  hover:scale-105 duration-300 "
           >
             <div class="h-full flex justify-start items-center row-span-2">
               <img
@@ -79,9 +79,17 @@ function Store() {
               <p class="font-chinese text-3xl tracking-wider"> Item name</p>
             </div>
             <div class="flex justify-center items-center">
-              <button class="border-solid border-2 border-black w-48 font-chinese text-3xl tracking-wider">
-                View more
-              </button>
+              <a href="#_" class="relative inline-block text-lg group">
+                <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                  <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                  <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                  <span class="relative">View More</span>
+                </span>
+                <span
+                  class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                  data-rounded="rounded-lg"
+                ></span>
+              </a>
             </div>
           </div>
         </div>
