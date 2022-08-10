8e602b7c66bd55a77f5bf43c2bbcf7adcd9223c4
aaronlrv <aaronvitharanage@gmail.com>
2022-08-10 10:32:27 +0400

    Allow routes to be active

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./Components/Homepage";
+import RouteSwitch from "./Components/RouteSwitch";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <RouteSwitch />
     </div>
   );
 }
