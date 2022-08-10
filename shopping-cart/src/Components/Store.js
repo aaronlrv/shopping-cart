1466b04bef245e32bccadde9d0548a72a35ecbd5
aaronlrv <aaronvitharanage@gmail.com>
2022-08-10 15:09:39 +0400

    Add header

--- shopping-cart/src/Components/Store.js
+++ shopping-cart/src/Components/Store.js
@@ -19,8 +19,15 @@ function Store() {
   }, []);
 
   return (
-    <div>
-      <p>HI! This is the Store page</p>
+    <div class="h-screen">
+      <div className="header" class="bg-[rgba(112,94,191,0.5)] h-20 shadow-lg">
+        <div
+          className="header-text"
+          class="h-full w-full flex justify-center items-center"
+        >
+          <p class="font-oswald text-3xl tracking-wider">Daily Store</p>
+        </div>
+      </div>
     </div>
   );
 }
