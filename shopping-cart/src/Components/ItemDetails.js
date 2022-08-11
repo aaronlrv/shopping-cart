aa7e95f9530e2f7a3e553683416db305abbbf6ec
aaronlrv <aaronvitharanage@gmail.com>
2022-08-11 16:43:13 +0400

    Add header

--- shopping-cart/src/Components/ItemDetails.js
+++ shopping-cart/src/Components/ItemDetails.js
@@ -1,8 +1,30 @@
-function ItemDetails() {
+function ItemDetails({ match }) {
   return (
-    <div>
-      <div>
-        <p> hi</p>
+    <div
+      className="header-nav"
+      class="  bg-[#1a181aff] h-20 shadow-lg col-span-2"
+    >
+      <div class="flex flex-row items-center w-full h-full">
+        <div
+          className="header-text"
+          class="h-full w-full flex justify-center items-center"
+        >
+          <p class="font-oswald text-3xl tracking-wider text-white">
+            Daily Store
+          </p>
+        </div>
+        <div classname="links" class="pr-12 h-full w-auto mt-0">
+          <ul class="flex flex-row gap-20  justify-center items-center h-full text-3xl">
+            <li>
+              <div class="h-full items-center ">
+                <ion-icon name="cart-outline"></ion-icon>
+              </div>
+            </li>
+            <li>
+              <ion-icon name="menu-sharp"></ion-icon>{" "}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
