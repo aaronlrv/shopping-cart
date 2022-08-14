27874b9d3e93d3b8a07dc756ce3e01abb9511d52
aaronlrv <aaronvitharanage@gmail.com>
2022-08-14 15:50:39 +0400

    Remove buttons

--- shopping-cart/src/Components/ItemDetails.js
+++ shopping-cart/src/Components/ItemDetails.js
@@ -37,30 +37,29 @@ function ItemDetails({ match }) {
                 className="header-text"
                 class="h-full w-full flex justify-center items-center"
               >
-                <button class="bg-white" onClick={() => setTheme(colorTheme)}>
-                  Switch Themes
-                </button>
+                <div onClick={() => setTheme(colorTheme)}>
+                  {colorTheme === "light" && (
+                    <ion-icon name="moon-outline"></ion-icon>
+                  )}
 
-                {colorTheme === "light" && (
-                  <ion-icon name="moon-outline"></ion-icon>
-                )}
+                  {colorTheme === "dark" && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                      />
+                    </svg>
+                  )}
+                </div>
 
-                {colorTheme === "dark" && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                    />
-                  </svg>
-                )}
                 <Link to="/shop">
                   <p class="font-oswald text-3xl tracking-wider text-white">
                     Daily Store
