020f5661d8dfefb4013312e8879417f803b6ffe2
aaronlrv <aaronvitharanage@gmail.com>
2022-08-20 17:00:03 +0400

    Add quantity property to cart items

--- shopping-cart/src/Components/Cart.js
+++ shopping-cart/src/Components/Cart.js
@@ -118,11 +118,11 @@ function Cart({ cart }) {
 
           <button
             onClick={() => alert("Thank you for shopping!")}
-            class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter text-white bg-gray-800 rounded-lg group"
+            class="relative inline-flex items-center justify-center px-10 py-4 overflow-hidden font-mono font-medium tracking-tighter rounded-lg group bg-slate-50 text-black dark:text-white dark:bg-gray-800 "
           >
-            <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-green-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
-            <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-            <span class="relative">Button Text</span>
+            <span class="absolute w-0 h-0 transition-all duration-500 ease-out bg-blue-500 rounded-full group-hover:w-56 group-hover:h-56"></span>
+            <span class="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-400 dark:to-gray-700"></span>
+            <span class="relative">Checkout</span>
           </button>
         </div>
       </div>
--- shopping-cart/src/Components/RouteSwitch.js
+++ shopping-cart/src/Components/RouteSwitch.js
@@ -7,23 +7,68 @@ import ItemDetails from "./ItemDetails";
 import Cart from "./Cart";
 import { useState } from "react";
 
+///opsuedo
+/// clone the entire cart
+/// if item name matches another item in cart
+/// change quantity of clone cart to cart + 1
+/// else just ad the item with quantity of 1
+/// then set cart?
 function RouteSwitch() {
-  let [cart, setCart] = useState();
+  let [cart, setCart] = useState([]);
 
   function addToCart(item) {
-    setCart(...cart, item);
+    let cloneCart = [...cart];
+    console.log(item);
+    console.log(item.data.item.name);
+
+    if (cloneCart.length === 0) {
+      item.quantity = 1;
+      cloneCart.push(item);
+      setCart(cloneCart); /// if no items in cart dont run comparison
+    } else {
+      let newArr = cloneCart.map((x) => {
+        if (item.data.item.name === x.data.item.name) {
+          x.quantity = x.quantity + 1;
+          return x;
+        } else {
+          item.quantity = 1;
+          cloneCart.push(item);
+          return item;
+        }
+      });
+      console.log(newArr);
+      setCart(newArr);
+    }
   }
+
+  // cart.map((x) => {
+  //   x.quantity = 1;
+  // });
+  // let objCopy;
+  // function addToCart(item) {
+  //   cart.map((x) => {
+  //     if (item.data.item.name === x.data.item.name) {
+  //       objCopy = x;
+  //       objCopy.quantity = objCopy.quantity + 1;
+  //       /// add quantity
+  //     }
+  //   });
+  //   setCart([...cart, item]);
+  //}
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<Store />} />
-        <Route path="/shop/:id" element={<ItemDetails />} />
+        <Route
+          path="/shop/:id"
+          element={<ItemDetails addToCart={addToCart} />}
+        />
 
         <Route path="/unreleased" element={<Unreleased />} />
         <Route path="/popular" element={<Popular />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart cart={[...cart]} />} />
       </Routes>
     </BrowserRouter>
   );
