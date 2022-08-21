dec20f4eb4144d6bea9b41f78bfd93f47d6af3df
aaronlrv <aaronvitharanage@gmail.com>
2022-08-21 15:28:40 +0400

    Add header-text to card layout

--- shopping-cart/src/Components/Popular.js
+++ shopping-cart/src/Components/Popular.js
@@ -30,7 +30,7 @@ function Popular() {
           className="header-nav"
           class="h-20 shadow-lg col-span-2 bg-white dark:bg-[#1a181aff]             "
         >
-          <div class="flex flex-row items-center w-full h-full">
+          <div class="flex flex-row justify-center items-center w-full h-full">
             <div
               className="header-text"
               class="h-full w-full flex justify-center items-center"
@@ -41,102 +41,113 @@ function Popular() {
                 </p>
               </Link>
             </div>
-            <div classname="links" class="pr-12 h-full w-auto mt-0">
-              <ul class="flex flex-row gap-20  justify-center items-center h-full text-3xl">
-                <li>
-                  <div onClick={() => setTheme(colorTheme)}>
-                    {colorTheme === "light" && (
-                      <ion-icon
-                        class="text-white"
-                        name="moon-outline"
-                      ></ion-icon>
-                    )}
-
-                    {colorTheme === "dark" && (
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-8 w-8"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth={2}
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                        />
-                      </svg>
-                    )}
-                  </div>
-                </li>
-                <Link to="/cart">
+            <div classname="links" class="pr-12 h-full w-15 mt-0">
+              <ul class="flex flex-row   justify-center items-center h-full w-full text-3xl">
+                <div class="gap-x-20 flex flex-row w-full">
                   <li>
-                    <div class="h-full items-center ">
-                      <ion-icon
-                        class="text-black dark:text-white"
-                        name="cart-outline"
-                      ></ion-icon>
+                    <div onClick={() => setTheme(colorTheme)}>
+                      {colorTheme === "light" && (
+                        <ion-icon
+                          class="text-white"
+                          name="moon-outline"
+                        ></ion-icon>
+                      )}
+
+                      {colorTheme === "dark" && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          class="h-8 w-8"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                          />
+                        </svg>
+                      )}
                     </div>
                   </li>
-                </Link>
+                  <Link to="/cart">
+                    <li>
+                      <div class="h-full items-center ">
+                        <ion-icon
+                          class="text-black dark:text-white"
+                          name="cart-outline"
+                        ></ion-icon>
+                      </div>
+                    </li>
+                  </Link>
 
-                <Link to="/">
-                  <li>
-                    <ion-icon
-                      class="text-black dark:text-white"
-                      name="home-outline"
-                    ></ion-icon>
-                  </li>
-                </Link>
+                  <Link to="/">
+                    <li>
+                      <ion-icon
+                        class="text-black dark:text-white"
+                        name="home-outline"
+                      ></ion-icon>
+                    </li>
+                  </Link>
+                </div>
               </ul>
             </div>
           </div>
         </div>
 
-        <div class="grid grid-rows-2 grid-cols-3">
-          {items.map((x) => {
-            return (
-              <>
-                <Link to={`/shop/${x.itemId}`}>
-                  <div
-                    className="card"
-                    class="h-72 gap-6 m-5 grid grid-rows-[100,100] grid-cols-2 shadow-xl transition ease-in-out delay-100  hover:scale-105 duration-300 bg-slate-100 w-[30rem]  "
-                  >
-                    <div class="h-full flex justify-start items-center row-span-2">
-                      <img
-                        class="h-48 w-auto "
-                        src={x.images.transparent}
-                        alt="fortnite skin"
-                      />
-                    </div>
-                    <div class="flex flex-col justify-center items-center">
-                      <p class="font-chinese text-3xl tracking-wider text-black">
-                        {x.name}
-                      </p>
-                      <p class="text-">{x.description}</p>
-                    </div>
-                    <div class="flex justify-center items-center">
-                      <a
-                        href={x.itemId}
-                        class="relative inline-block text-lg group"
+        <div class="flex flex-col justify-center items-center gap-5 mt-5">
+          <div>
+            <h3 class="font-oswaldtracking-widest text-3xl">
+              Popular items in the past 60 minutes
+            </h3>
+          </div>
+          <div class=" h-full mt-8 flex justify-center items-center gap-72">
+            <div class="grid grid-rows-2 grid-cols-3 gap-12">
+              {items.map((x) => {
+                return (
+                  <>
+                    <Link to={`/shop/${x.itemId}`}>
+                      <div
+                        className="card"
+                        class="h-72 gap-6 m-5 grid grid-rows-[100,100] grid-cols-2 shadow-xl transition ease-in-out delay-100  hover:scale-105 duration-300 bg-slate-100 w-[30rem]  "
                       >
-                        <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-[#272AB0] 0 rounded-lg group-hover:text-white">
-                          <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                          <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#272AB0] group-hover:-rotate-180 ease"></span>
-                          <span class="relative">View More</span>
-                        </span>
-                        <span
-                          class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                          data-rounded="rounded-lg"
-                        ></span>
-                      </a>
-                    </div>
-                  </div>
-                </Link>
-              </>
-            );
-          })}
+                        <div class="h-full flex justify-start items-center row-span-2">
+                          <img
+                            class="h-48 w-auto "
+                            src={x.images.transparent}
+                            alt="fortnite skin"
+                          />
+                        </div>
+                        <div class="flex flex-col justify-center items-center">
+                          <p class="font-chinese text-3xl tracking-wider text-black">
+                            {x.name}
+                          </p>
+                          <p class="text-">{x.description}</p>
+                        </div>
+                        <div class="flex justify-center items-center">
+                          <a
+                            href={x.itemId}
+                            class="relative inline-block text-lg group"
+                          >
+                            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-[#272AB0] 0 rounded-lg group-hover:text-white">
+                              <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                              <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#272AB0] group-hover:-rotate-180 ease"></span>
+                              <span class="relative">View More</span>
+                            </span>
+                            <span
+                              class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                              data-rounded="rounded-lg"
+                            ></span>
+                          </a>
+                        </div>
+                      </div>
+                    </Link>
+                  </>
+                );
+              })}
+            </div>
+          </div>
         </div>
 
         {/* <div
