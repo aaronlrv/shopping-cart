09f3d8f2d673bf108b402c1b3dcee8a98766ef44
aaronlrv <aaronvitharanage@gmail.com>
2022-08-22 21:46:50 +0400

    Add dark mode to popular page

--- shopping-cart/src/Components/Popular.js
+++ shopping-cart/src/Components/Popular.js
@@ -24,7 +24,7 @@ function Popular() {
   }, []);
 
   return (
-    <div class="">
+    <div class="dark:bg-[#0c0c0fff] h-screen">
       <div class="  ">
         <div
           className="header-nav"
@@ -95,57 +95,60 @@ function Popular() {
             </div>
           </div>
         </div>
-
-        <div class="flex flex-col justify-center items-center gap-5 mt-5">
-          {/* <div>
+        <div class="dark:bg-[#0c0c0fff] h-full">
+          <div class="flex flex-col justify-center items-center gap-5 mt-5  dark:bg-[#0c0c0fff]">
+            {/* <div>
             <h3 class="font-oswaldtracking-widest text-3xl">
               Popular items in the past 60 minutes
             </h3> */}
-        </div>
-        <div class=" h-full mt-8 flex justify-center items-center gap-72">
-          <div class="grid grid-rows-2 grid-cols-3 gap-12">
-            {items.map((x) => {
-              return (
-                <>
-                  <Link to={`/popular/${x.identifier}`}>
-                    <div
-                      className="card"
-                      class="h-72 gap-6 m-5 grid grid-rows-[100,100] grid-cols-2 shadow-xl transition ease-in-out delay-100  hover:scale-105 duration-300 bg-slate-100 w-[30rem]  "
-                    >
-                      <div class="h-full flex justify-start items-center row-span-2">
-                        <img
-                          class="h-48 w-auto "
-                          src={x.images.transparent}
-                          alt="fortnite skin"
-                        />
+          </div>
+          <div class=" h-full mt-8 flex justify-center items-center gap-72  dark:bg-[#0c0c0fff]">
+            <div class="grid grid-rows-2 grid-cols-3 gap-12">
+              {items.map((x) => {
+                return (
+                  <>
+                    <Link to={`/popular/${x.identifier}`}>
+                      <div
+                        className="card"
+                        class="h-72 gap-6 m-5 grid grid-rows-[100,100] grid-cols-2 shadow-xl transition ease-in-out delay-100  hover:scale-105 duration-300 bg-slate-100 w-[30rem] dark:bg-[#1a181aff]  "
+                      >
+                        <div class="h-full flex justify-start items-center row-span-2">
+                          <img
+                            class="h-48 w-auto "
+                            src={x.images.transparent}
+                            alt="fortnite skin"
+                          />
+                        </div>
+                        <div class="flex flex-col justify-center items-center">
+                          <p class="font-chinese text-3xl tracking-wider text-black dark:text-white">
+                            {x.name}
+                          </p>
+                          <p class="text-black dark:text-blue-200">
+                            {x.description}
+                          </p>
+                        </div>
+                        <div class="flex justify-center items-center">
+                          <a
+                            href={x.identifier}
+                            class="relative inline-block text-lg group"
+                          >
+                            <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-[#272AB0] 0 rounded-lg group-hover:text-white">
+                              <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                              <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#272AB0] group-hover:-rotate-180 ease"></span>
+                              <span class="relative">View More</span>
+                            </span>
+                            <span
+                              class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                              data-rounded="rounded-lg"
+                            ></span>
+                          </a>
+                        </div>
                       </div>
-                      <div class="flex flex-col justify-center items-center">
-                        <p class="font-chinese text-3xl tracking-wider text-black">
-                          {x.name}
-                        </p>
-                        <p class="text-">{x.description}</p>
-                      </div>
-                      <div class="flex justify-center items-center">
-                        <a
-                          href={x.identifier}
-                          class="relative inline-block text-lg group"
-                        >
-                          <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-[#272AB0] 0 rounded-lg group-hover:text-white">
-                            <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-                            <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#272AB0] group-hover:-rotate-180 ease"></span>
-                            <span class="relative">View More</span>
-                          </span>
-                          <span
-                            class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                            data-rounded="rounded-lg"
-                          ></span>
-                        </a>
-                      </div>
-                    </div>
-                  </Link>
-                </>
-              );
-            })}
+                    </Link>
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
