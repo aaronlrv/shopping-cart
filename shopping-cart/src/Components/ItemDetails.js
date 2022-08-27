987468bc32044abed94c544f66b3687cbf470dcc
aaronlrv <aaronvitharanage@gmail.com>
2022-08-27 15:12:51 +0400

    Add responsive header

--- shopping-cart/src/Components/ItemDetails.js
+++ shopping-cart/src/Components/ItemDetails.js
@@ -38,73 +38,58 @@ function ItemDetails({ addToCart, price }) {
     return (
       <div class="">
         {" "}
-        <div class=" h-screen grid grid-rows-[80px,1fr] grid-cols-[0.66fr,1fr]">
+        <div class=" h-screen grid grid-rows-[80px,1fr] grid-cols-[0.66fr,1fr] ">
           <div
             className="header-nav"
-            class="h-20 shadow-lg col-span-2 bg-white dark:bg-[#1a181aff]             "
+            class="h-20 fixed top-0 shadow-md bg-white dark:bg-[#1a181aff] w-full flex justify-between items-center pl-6 md:pl-24 z-30"
           >
-            <div class="flex flex-row items-center w-full h-full">
-              <div
-                className="header-text"
-                class="h-full w-full flex justify-center items-center"
+            <Link to="/shop">
+              <p class="font-oswald text-3xl tracking-wider text-black dark:text-white">
+                Daily Store
+              </p>
+            </Link>
+            <ul classname="links" class="flex list-none">
+              <li
+                onClick={() => setTheme(colorTheme)}
+                className="text-3xl pr-6 md:pr-24"
               >
-                <Link to="/shop">
-                  <p class="font-oswald text-3xl tracking-wider text-black dark:text-white">
-                    Daily Store
-                  </p>
+                {colorTheme === "light" && (
+                  <ion-icon class="text-white" name="moon-outline"></ion-icon>
+                )}
+                {colorTheme === "dark" && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-8 w-8"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                    />
+                  </svg>
+                )}
+              </li>
+              <li className="text-3xl pr-6 md:pr-24">
+                <Link to="/cart">
+                  <ion-icon
+                    class="text-black dark:text-white"
+                    name="cart-outline"
+                  ></ion-icon>
                 </Link>
-              </div>
-              <div classname="links" class="pr-12 h-full w-auto mt-0">
-                <ul class="flex flex-row gap-20  justify-center items-center h-full text-3xl">
-                  <li>
-                    <div onClick={() => setTheme(colorTheme)}>
-                      {colorTheme === "light" && (
-                        <ion-icon
-                          class="text-white"
-                          name="moon-outline"
-                        ></ion-icon>
-                      )}
-
-                      {colorTheme === "dark" && (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-8 w-8"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                          />
-                        </svg>
-                      )}
-                    </div>
-                  </li>
-                  <Link to="/cart">
-                    <li>
-                      <div class="h-full items-center ">
-                        <ion-icon
-                          class="text-black dark:text-white"
-                          name="cart-outline"
-                        ></ion-icon>
-                      </div>
-                    </li>
-                  </Link>
-
-                  <Link to="/">
-                    <li>
-                      <ion-icon
-                        class="text-black dark:text-white"
-                        name="home-outline"
-                      ></ion-icon>
-                    </li>
-                  </Link>
-                </ul>
-              </div>
-            </div>
+              </li>
+              <li className="text-3xl pr-6 md:pr-24">
+                <Link to="/">
+                  <ion-icon
+                    class="text-black dark:text-white"
+                    name="home-outline"
+                  ></ion-icon>
+                </Link>
+              </li>
+            </ul>
           </div>
           <div className="photo" class=" bg-[rgba(39,42,176,0.2)] row-span-2">
             <div class="flex justify-center items-center h-full">
