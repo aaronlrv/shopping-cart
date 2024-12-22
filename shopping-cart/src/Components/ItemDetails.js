d5499dfddbe3b108733d7ed5c6e0233c3db95b51
aaron <alr200@hw.ac.uk>
2024-12-22 13:17:08 +0400

    Conditionally render add to cart button based on whether it's currently in the market or not

--- shopping-cart/src/Components/ItemDetails.js
+++ shopping-cart/src/Components/ItemDetails.js
@@ -167,19 +167,21 @@ function ItemDetails({ addToCart, showPrice }) {
                   </p>
                 )}
 
-                <div class="flex justify-start- items-center pt-2 pb-2 ">
-                  <button
-                    onClick={() => addToCart(item)}
-                    class="relative inline-flex items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
-                  >
-                    <span class="w-36 h-36 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
-                    <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
-                    <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
-                      Add to cart
-                    </span>
-                    <span class="absolute inset-0 border-2 border-white rounded-full"></span>
-                  </button>
-                </div>
+                {showPrice && price && price !== "N/A" && (
+                  <div class="flex justify-start items-center pt-2 pb-2">
+                    <button
+                      onClick={() => addToCart(item)}
+                      class="relative inline-flex items-center justify-start inline-block px-5 py-3 overflow-hidden font-bold rounded-full group"
+                    >
+                      <span class="w-36 h-36 rotate-45 translate-x-12 -translate-y-2 absolute left-0 top-0 bg-white opacity-[3%]"></span>
+                      <span class="absolute top-0 left-0 w-48 h-48 -mt-1 transition-all duration-500 ease-in-out rotate-45 -translate-x-56 -translate-y-24 bg-white opacity-100 group-hover:-translate-x-8"></span>
+                      <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-gray-900">
+                        Add to cart
+                      </span>
+                      <span class="absolute inset-0 border-2 border-white rounded-full"></span>
+                    </button>
+                  </div>
+                )}
               </div>
               {/*
               <div class="flex mt-28 h-36">
